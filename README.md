c1a336850380f9d7b370d65e7e6c9603fd36975f
jmanteau <undisclosed@mail.org>
2012-09-10 11:12:56 +0200

    Ajout Kibana et Logstash

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ The logs are under the dir /var/log/remote/$HOSTNAME/$YEAR/$MONTH/$DAY-log
 Tested on a Debian Squeeze. Should work on others distros.
 
 Usage:
-# wget https://raw.github.com/jmanteau/simple-central-rsyslog/master/simple-central-rsyslog.sh
-# sh simple-central-rsyslog.sh
+\# wget --no-check-certificatehttps://raw.github.com/jmanteau/simple-central-rsyslog/master/simple-central-rsyslog.sh
+\# bash simple-central-rsyslog.sh
--- simple-central-rsyslog.sh
+++ simple-central-rsyslog.sh
@@ -19,13 +19,20 @@ VERSION="1.0"
 WGET="wget -m --no-check-certificate"
 DATE=`date +"%Y%m%d%H%M%S"`
 LOG_FILE="/tmp/simple-central-rsyslog-$DATE.log"
+APT="aptitude -q -y"
 
+# UPGRADE both of them !
+LOGSTASHURL="http://semicomplete.com/files/logstash/logstash-1.1.1-monolithic.jar"
+ELASTICSEARCHDEB="https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.8.deb"
 
+LOGSTASHDIR="/opt/logstash"
+LOGSTASHINIT="/etc/init.d/logstash"
+LOGSTASHINITURL="https://raw.github.com/gist/3623477/377c1ec852a2d956f152e032a95644b3f17b5bb4/logstash.sh"
 CONFRSYSLOG="https://raw.github.com/jmanteau/simple-central-rsyslog/master/rsyslog.conf"
 CONFCRON="https://raw.github.com/jmanteau/simple-central-rsyslog/master/rsyslog-bzip2.txt"
 REMOTELOGDIR="/var/log/remote/"
 
-# Fonctions utilisées par le script
+# Fonctions 
 #---------------------------------
 
 displaymessage() {
@@ -55,7 +62,7 @@ displayerrorandexit() {
 # Autres parametres: COMMAND
 displayandexec() {
   local message=$1
-  echo -n "[En cours] $message"
+  echo -n "[In progress] $message"
   shift
   echo ">>> $*" >> $LOG_FILE 2>&1
   sh -c "$*" >> $LOG_FILE 2>&1
@@ -68,25 +75,44 @@ displayandexec() {
   return $ret
 }
 
-# Debut du programme
+# Start
 #-------------------
 
-# Test que le script est lance en root
+# are you root ?
 if [ $EUID -ne 0 ]; then
-  displayerror 1 "Le script doit être lancé en root: # su - -c $0"
+  displayerror 1 "You have to be root: # su - -c $0"
 fi
 
-# Création du fichier de log
-echo "Debut du script" > $LOG_FILE
+# Log
+echo "Starting" > $LOG_FILE
 
-displaytitle "-- Installation des fichiers et redémarrage du daemon"
-displayandexec "Téléchargement de la configuration rsyslog" $WGET -O /etc/rsyslog.conf $CONFRSYSLOG
-displayandexec "Téléchargement de la configuration cron pour la compression" $WGET -O /etc/cron.daily/rsyslog-bzip2 $CONFCRON
-displayandexec "Changement des droits du cron pour la compression" chmod +x /etc/cron.daily/rsyslog-bzip2
-displayandexec "Création du répertoire de log $REMOTELOGDIR" mkdir -p $REMOTELOGDIR
-displayandexec "Redémarrage de rsyslog" /etc/init.d/rsyslog restart
-displaytitle "-- Fin d'installation"
+displaytitle "-- RSYSLOG"
+displayandexec "Downloading rsyslog configuration" $WGET -O /etc/rsyslog.conf $CONFRSYSLOG
+displayandexec "Downloading cron configuration to compress logs" $WGET -O /etc/cron.daily/rsyslog-bzip2 $CONFCRON
+displayandexec "Adjusting rights" chmod +x /etc/cron.daily/rsyslog-bzip2
+displayandexec "Making remote log dir" mkdir -p $REMOTELOGDIR
+displayandexec "Restarting rsyslog" /etc/init.d/rsyslog restart
+displaytitle "-- Done"
 
-echo "Fin du script" >> $LOG_FILE
+displaytitle "-- LOGSTASH" 
+displayandexec "Making Logstash dirs" mkdir -p $LOGSTASHDIR && mkdir /var/log/logstash && mkdir /etc/logstash
+displayandexec "Downloading Logstash jar" $WGET -O $LOGSTASHDIR/logstash.jar $LOGSTASHURL
+displayandexec "Installing Java" $APT install default-jre
+displayandexec "Downloading logstash init.d" $WGET -O $LOGSTASHINIT $LOGSTASHINITURL
+displayandexec "Adjusting rights" chmod +x $LOGSTASHINIT
+displayandexec "Starting at boot" update-rc.d logstash defaults
+
+# In case of distributed elasticsearch
+#displaytitle "-- ELASTICSEARCH"
+#displayandexec "Downloading deb" $WGET $ELASTICSEARCHDEB
+#displayandexec "Installing Elasticsearch from deb" dpkg -i elasticsearch*.deb
+
+
+displaytitle "-- KIBANA"
+displayandexec "Installing requirement" $APT install git ruby rubygems apache2 curl libcurl3-dev
+displayandexec "Installing Ruby stuff" export PATH=/var/lib/gems/1.8/bin/:${PATH} && gem install bundler
+displayandexec "Downloading Kibana" cd /var/www/ &&  git clone --branch=kibana-ruby https://github.com/rashidkpc/Kibana.git 
+displayandexec "Installing Ruby gems" cd /var/www/Kibana && bundle install
+echo "End" >> $LOG_FILE
 
 
